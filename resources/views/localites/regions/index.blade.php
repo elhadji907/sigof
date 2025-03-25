55a1e4517e9ee5ea9f907ff4971e6598054367a5
elhadji907 <badjilaminefbkk@gmail.com>
2025-03-25 09:20:23 +0000

    demandes par antenne

--- resources/views/localites/regions/index.blade.php
+++ resources/views/localites/regions/index.blade.php
@@ -326,7 +326,7 @@
         new DataTable('#table-regions', {
             layout: {
                 topStart: {
-                    buttons: ['copy', 'csv', 'excel', 'pdf', 'print'],
+                    buttons: ['csv', 'excel', 'print'],
                 }
             },
             "order": [
